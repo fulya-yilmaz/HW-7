eedd889ad64eeec8fcd18b4b44faec57d448ed6f
Tim <13306362+timmarschall@users.noreply.github.com>
2025-04-28 20:22:20 +0200

    style: format everything

--- script.js
+++ script.js
@@ -1,74 +1,69 @@
 const menuDatabase = [
-    [
-      "Papadum",
-      20,
-      "vegetarian",
-      "https://www.shutterstock.com/image-photo/deep-fried-khichiya-papad-traditional-260nw-1076490656.jpg",
-    ],
-    [
-      "Pakora",
-      50,
-      "meat",
-      "https://www.indianhealthyrecipes.com/wp-content/uploads/2022/02/vegetable-pakora-recipe.jpg",
-    ],
-    [
-      "Tandoori Chicken",
-      60,
-      "meat",
-      "https://www.cubesnjuliennes.com/wp-content/uploads/2022/12/Tandoori-Chicken-Recipe.jpg",
-    ],
-    [
-      "Samosa",
-      50,
-      "vegetarian",
-      "https://img.mummum.dk/wp-content/uploads/2022/03/IMG_9814-min.jpg",
-    ],
-  ];
+  [
+    "Papadum",
+    20,
+    "vegetarian",
+    "https://www.shutterstock.com/image-photo/deep-fried-khichiya-papad-traditional-260nw-1076490656.jpg",
+  ],
+  [
+    "Pakora",
+    50,
+    "meat",
+    "https://www.indianhealthyrecipes.com/wp-content/uploads/2022/02/vegetable-pakora-recipe.jpg",
+  ],
+  [
+    "Tandoori Chicken",
+    60,
+    "meat",
+    "https://www.cubesnjuliennes.com/wp-content/uploads/2022/12/Tandoori-Chicken-Recipe.jpg",
+  ],
+  [
+    "Samosa",
+    50,
+    "vegetarian",
+    "https://img.mummum.dk/wp-content/uploads/2022/03/IMG_9814-min.jpg",
+  ],
+];
 
-  displayMenu(menuDatabase);
+displayMenu(menuDatabase);
 
-  function showVegetarian(){
-    let menuItems=menuDatabase.filter(menuItem=>{
-         return menuItem[2]=='vegetarian'
-    });
-    displayMenu(menuItems);
-  }
+function showVegetarian() {
+  let menuItems = menuDatabase.filter((menuItem) => {
+    return menuItem[2] == "vegetarian";
+  });
+  displayMenu(menuItems);
+}
 
-  function showMeat(){
-    let menuItems=menuDatabase.filter(menuItem=>{
-         return menuItem[2]=='meat'
-    });
-    displayMenu(menuItems);
-  }
+function showMeat() {
+  let menuItems = menuDatabase.filter((menuItem) => {
+    return menuItem[2] == "meat";
+  });
+  displayMenu(menuItems);
+}
 
+function displayMenu(menuItems) {
+  let listContainer = document.getElementById("list-container");
 
-  function displayMenu(menuItems){
+  menuItems.forEach((menuItem) => {
+    let liElement = document.createElement("li");
 
-    let listContainer= document.getElementById('list-container');
- 
+    let imageElement = document.createElement("img");
+    imageElement.src = menuItem[3];
 
-    menuItems.forEach(menuItem => {
-        
-        let liElement = document.createElement('li');
+    let productNameElement = document.createElement("p");
+    productNameElement.innerText = menuItem[0];
 
-        let imageElement = document.createElement('img');
-        imageElement.src = menuItem[3];
+    let priceElement = document.createElement("p");
+    priceElement.innerText = menuItem[1];
 
-        let productNameElement = document.createElement('p');
-        productNameElement.innerText=menuItem[0];
+    let categoryElement = document.createElement("p");
+    categoryElement.innerText = menuItem[2];
 
-        let priceElement = document.createElement('p');
-        priceElement.innerText=menuItem[1];
+    liElement.appendChild(imageElement);
+    liElement.appendChild(productNameElement);
+    liElement.appendChild(priceElement);
+    liElement.appendChild(categoryElement);
 
-        let categoryElement = document.createElement('p');
-        categoryElement.innerText=menuItem[2];
-
-        liElement.appendChild(imageElement);
-        liElement.appendChild(productNameElement);
-        liElement.appendChild(priceElement);
-        liElement.appendChild(categoryElement);
-
-        listContainer.appendChild(liElement);
-    });
-
-  }
+    listContainer.appendChild(liElement);
+  });
+}
